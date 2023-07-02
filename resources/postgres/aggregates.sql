c5f0edcacd00439ed083f4647b6d9412ca01661f
Alexander Pohl <alex@ahpohl.com>
2023-07-02 21:51:24 +0200

    initial commit

---
+++ resources/postgres/aggregates.sql
@@ -0,0 +1,56 @@
+\c fronius
+
+--
+-- daily continuous aggregate
+--
+DROP MATERIALIZED VIEW IF EXISTS cagg_daily CASCADE;
+
+CREATE MATERIALIZED VIEW cagg_daily
+WITH (timescaledb.continuous, timescaledb.materialized_only=true) 
+AS
+SELECT
+  time_bucket('1 day', time) AS bucket_1d,
+  sensor_id,
+  plan_id,
+  last(total_energy, time) - first(total_energy, time) AS energy_1d,
+  first(total_energy, time) AS total,
+  avg(grid_power) AS power_avg,
+  max(grid_power) AS power_max
+FROM live
+GROUP BY bucket_1d, plan_id, sensor_id
+WITH NO DATA;
+
+-- refresh policy
+SELECT add_continuous_aggregate_policy('cagg_daily',
+  start_offset => INTERVAL '3 days',
+  end_offset => INTERVAL '1 hour',
+  schedule_interval => '1 day');
+
+--
+-- average power
+--
+DROP MATERIALIZED VIEW IF EXISTS cagg_power CASCADE;
+
+CREATE MATERIALIZED VIEW cagg_power
+WITH (timescaledb.continuous, timescaledb.materialized_only=true)
+AS
+SELECT
+  time_bucket('5 minutes', time) as bucket_5m,
+  avg(grid_power) as power_5m,
+  avg(power_p1) as power_p1,
+  avg(power_p2) as power_p2
+FROM live
+GROUP BY bucket_5m           
+WITH NO DATA;
+
+-- refresh policy
+SELECT add_continuous_aggregate_policy('cagg_power',
+  start_offset => INTERVAL '3 hours',
+  end_offset => INTERVAL '1 minute',
+  schedule_interval => '5 minutes');
+
+-- retention policy
+SELECT add_retention_policy('cagg_power', INTERVAL '3 days');
+
+-- grant
+GRANT SELECT ON TABLE cagg_power TO grafana;
---
+++ resources/postgres/views.sql
@@ -0,0 +1,88 @@
+\c fronius
+
+-- create daily view with data from archive and cagg_daily
+DROP MATERIALIZED VIEW IF EXISTS daily_view CASCADE;
+
+CREATE MATERIALIZED VIEW daily_view
+AS
+SELECT
+  bucket_1d AS time,
+  energy_1d AS energy,
+  energy_1d * payment AS credit,
+  total,
+  payment,
+  power_avg,
+  power_max
+FROM archive JOIN plan ON archive.plan_id = plan.id
+GROUP BY bucket_1d, energy_1d, total, payment, power_avg, power_max
+UNION
+SELECT
+  bucket_1d AS time,
+  energy_1d AS energy,
+  energy_1d * payment AS credit,
+  total,
+  payment,
+  power_avg,
+  power_max
+FROM cagg_daily JOIN plan ON cagg_daily.plan_id = plan.id
+-- insert end time of archive
+WHERE bucket_1d > TIMESTAMP WITH TIME ZONE '2023-01-10 01:00:00+01'
+GROUP BY bucket_1d, energy_1d, total, payment, power_avg, power_max
+ORDER BY time;
+
+-- index
+CREATE UNIQUE INDEX daily_idx ON daily_view (time);
+
+-- grant
+GRANT SELECT ON TABLE daily_view TO grafana;
+
+--
+-- create monthly view
+--
+CREATE MATERIALIZED VIEW monthly_view
+AS
+SELECT
+  time_bucket('1 month', time) AS time,
+  sum(energy) AS energy_sum,
+  sum(credit) AS credit,
+  first(total, time) AS total,
+  min(energy) AS energy_min,
+  avg(energy) AS energy_avg,
+  max(energy) AS energy_max,
+  avg(power_avg) AS power_avg,
+  max(power_max) AS power_max
+FROM daily_view
+GROUP BY time_bucket('1 month', time)
+ORDER BY time;
+
+-- index
+CREATE UNIQUE INDEX monthly_idx ON monthly_view (time);
+
+-- grant
+GRANT SELECT ON TABLE monthly_view TO grafana;
+
+--
+-- create yearly view
+--
+CREATE MATERIALIZED VIEW yearly_view
+AS
+SELECT
+  time_bucket('1 year', time) AS time,
+  count(*) as days,
+  sum(energy) AS energy_sum,
+  sum(credit) AS credit,
+  first(total, time) AS total,
+  min(energy) AS energy_min,
+  avg(energy) AS energy_avg,
+  max(energy) AS energy_max,
+  avg(power_avg) AS power_avg,
+  max(power_max) AS power_max
+FROM daily_view
+GROUP BY time_bucket('1 year', time)
+ORDER BY time;
+
+-- index
+CREATE UNIQUE INDEX yearly_idx ON yearly_view (time);
+
+-- grant
+GRANT SELECT ON TABLE yearly_view TO grafana;
